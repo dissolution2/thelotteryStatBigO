60575e00e039ff8ba8bde6be3f474240096c54d5
dissolution2 <larsen.robin75@gmail.com>
2022-10-06 04:48:48 +0200

    refaction added Class

--- mainScript.js
+++ mainScript.js
@@ -16,10 +16,149 @@ let vikingTall = new Map(); // 1 - 5
 let map = new Map(); // 
 let numberFrequensVikingMainLotto = []; // We can change from array with Map -to only Map !!?? number, value freq but this is to test and learn
 let winningVikingRow = []; // store all the winning numbers 
-
-
 let numberFrequensVikingTall  = []; 
+let numberSetsFrequensVikingTall = new Map();
 
+/** value number 24 
+ * WinningRow index 0
+ *  befor 33 after 2
+ * WinningRow index 1
+ *  befor 48 after 42
+ * 
+ * value = 24
+ * befor.set(33,0)
+ * after.set(2,0)
+ * 
+ * befor.set(48,0)
+ * after.set(42,0)
+ * 
+ * if(befor.has(key))
+ *  freqNumber = befor.get(key)
+ *  befor.set(key,freqNubmer + 1)
+ * 
+ * ?
+ * both ways !? so we have then every drawn number befor and after and there frequency drawn in that order.
+ * befor.set(2,0)
+ * after.set(48,0)
+ * 
+ */
+
+class numberFrequensSetsClass{
+    constructor(value){
+        this.numberValue = value;
+        this.befor = new Map();
+        this.after = new Map();
+    }
+
+    getNumberValue(){
+        return this.numberValue;
+    }
+
+    findBefor(valueToFind){
+        for(let i=0; i < this.befor.size; i++){
+            if(this.befor.has(valueToFind)){
+                return true;
+            }
+        }
+        return false; 
+    }
+    
+    findAfter(valueToFind){
+        for(let i=0; i < this.after.size; i++){
+            if(this.after.has(valueToFind)){
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    addBeforValue(beforV){
+
+        switch(this.findBefor(beforV)){
+            case true:
+                this.befor.set(beforV, this.befor.get(beforV) + 1);
+                break;
+            case false:
+                this.befor.set(beforV,1);
+                break;
+        }
+        
+    }
+
+    addAfterValue(afterV){
+
+        switch(this.findAfter(afterV)){
+            case true:
+                this.after.set(afterV, this.after.get(afterV) + 1);
+                break;
+            case false:
+                this.after.set(afterV,1);
+                break;
+        }
+        
+    }
+
+    getBeforValue(){
+        for (var [key, value] of this.befor) {
+            console.log("Befor Number: " + key + " Frequesn drawn: " + value);
+        }
+    }
+
+    getAfterValue(){
+        for (var [key, value] of this.after) {
+            console.log("After Number: " + key + " Frequesn drawn: " + value);
+        }
+    }
+
+
+}
+
+for(let y=1; y < 49; y++){
+    numberSetsFrequensVikingTall.set(y,new numberFrequensSetsClass(y));
+}
+function test_numberSetsFrequensVikingTall(value, beforV, afterV){
+
+    if(numberSetsFrequensVikingTall.has(value)){
+        
+        /** beforV */
+        //console.log("set main numbers with nuberFrqClass empty Map's")
+        numberSetsFrequensVikingTall.get(value).addBeforValue(beforV);
+        /** Print Check Value -on error!! */
+        //console.log("MainLottoNumber: " + numberSetsFrequensVikingTall.get(value).getNumberValue());
+        //numberSetsFrequensVikingTall.get(value).getBeforValue();
+        
+        /** afterV */
+        numberSetsFrequensVikingTall.get(value).addAfterValue(afterV);
+        /** Print Check Value -on error!! */
+        //console.log("MainLottoNumber: " + numberSetsFrequensVikingTall.get(value).getNumberValue());
+        //numberSetsFrequensVikingTall.get(value).getAfterValue();
+
+    }
+}
+
+function printAllSets(){
+    for(let y=1; y < 49; y++){
+        console.log("MainLottoNumber: "+  numberSetsFrequensVikingTall.get(y).getNumberValue());
+        numberSetsFrequensVikingTall.get(y).getBeforValue();
+        numberSetsFrequensVikingTall.get(y).getAfterValue();
+    }
+}
+
+function printLookUpFreqOfaMainNumber(value){
+    console.log("MainLottoNumber: "+  numberSetsFrequensVikingTall.get(value).getNumberValue());
+    numberSetsFrequensVikingTall.get(value).getBeforValue();
+    numberSetsFrequensVikingTall.get(value).getAfterValue();
+}
+
+/** testing Class */
+//test_numberSetsFrequensVikingTall(24,2,33);
+//test_numberSetsFrequensVikingTall(24,4,33);
+//test_numberSetsFrequensVikingTall(24,4,33); // dublicate 4 freq = 2
+//test_numberSetsFrequensVikingTall(25,1,33);
+//test_numberSetsFrequensVikingTall(25,6,33);
+//test_numberSetsFrequensVikingTall(25,2,33);
+//test_numberSetsFrequensVikingTall(25,6,33); // dublicate 6 freq = 2
+//printAllSets();
 
 
 
@@ -108,6 +247,12 @@ function inFunc_setVikingLottoWinningRowToFreq(n_1, n_2, n_3, n_4, n_5, n_6, n_7
     }
 }
 
+function inFunc_setsFrequenseOfSets(){
+    // use winningVikingRow
+    // eks 
+    test_numberSetsFrequensVikingTall(24,2,33);
+}
+
 function dateFunction(arg){
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
@@ -132,9 +277,14 @@ function inFunc_printVikingLottoWinningRows(){
 }
 
 /** Add the winning rows */
-inFunc_setVikingLottoWinningRowToFreq(15,48,24,42,7,13,2);
-inFunc_setVikingLottoWinningRowToFreq(6,9,19,24,27,35,3);
-inFunc_setVikingLottoWinningRowToFreq(7,34,21,18,35,11,4);
+
+inFunc_setVikingLottoWinningRowToFreq(6,9,19,24,27,35,3); //
+inFunc_setVikingLottoWinningRowToFreq(7,34,21,18,35,11,4); //
+
+
+inFunc_setVikingLottoWinningRowToFreq(15,48,24,42,7,13,2); // Ons 21 sep
+inFunc_setVikingLottoWinningRowToFreq(33,24,2,42,17,44,2) // Ons 28 sep
+inFunc_setVikingLottoWinningRowToFreq(5,45,6,35,11,31,1); // Ons 5 okt
 
 function lookUpStats(menuArg, arg){
 
@@ -165,13 +315,21 @@ function menu(){
   "lookUpStats(MenuArg, arg to the list) - 0 to get list menu\n\n");
 }
 //eks
-//menu();
-//lookUpStats(0);
+menu();
+lookUpStats(0);
+test_numberSetsFrequensVikingTall(24,2,33);
+printLookUpFreqOfaMainNumber(24);
+
+
+
+
 //lookUpStats(3);
 //lookUpStats(1,24);
 //lookUpStats(2,1);
 //lookUpStats(2,2);
-inFunc_printVikingLottoWinningRows();
+
+
+
 
 
 
