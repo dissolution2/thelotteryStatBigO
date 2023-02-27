9dad245496b2ea188b82048bb2afed40bb181ea8
dissolution2 <larsen.robin75@gmail.com>
2023-02-27 14:53:40 +0100

    test on remote rep

--- winnin_row_file.js
+++ winnin_row_file.js
@@ -1,11 +1,26 @@
-inFunc_setVikingLottoWinningRowToFreq(6,9,19,24,27,35,3); //
-inFunc_setVikingLottoWinningRowToFreq(7,34,21,18,35,11,4); //
-inFunc_setVikingLottoWinningRowToFreq(15,48,24,42,7,13,2); // Ons 21 sep
-inFunc_setVikingLottoWinningRowToFreq(33,24,2,42,17,44,2) // Ons 28 sep
-inFunc_setVikingLottoWinningRowToFreq(5,45,6,35,11,31,1); // Ons 5 okt
-inFunc_setVikingLottoWinningRowToFreq(37,18,40,21,19,7,3) // 12 okt
-inFunc_setVikingLottoWinningRowToFreq(35,4,44,24,17,41,2) // 19 oktber
-inFunc_setVikingLottoWinningRowToFreq(17,1,22,23,45,5,5); // Ons 2 Nov
-inFunc_setVikingLottoWinningRowToFreq(22,17,10,18,15,23,3); // Ons 9 Nov
-inFunc_setVikingLottoWinningRowToFreq(30,46,37,41,9,44,2); // Ons 16 Nov
-inFunc_setVikingLottoWinningRowToFreq(32,28,27,12,37,5,4); // Ons 23 nov
+
+
+inFunc_setVikingLottoWinningRowToFreq(6,9,19,24,27,35,3,"February 7,2021"); 
+inFunc_setVikingLottoWinningRowToFreq(7,34,21,18,35,11,4,"February 20,2021");
+inFunc_setVikingLottoWinningRowToFreq(15,48,24,42,7,13,2,"September 21,2022");
+inFunc_setVikingLottoWinningRowToFreq(33,24,2,42,17,44,2,"September 28,2022");
+inFunc_setVikingLottoWinningRowToFreq(5,45,6,35,11,31,1,"October 5,2022"); 
+inFunc_setVikingLottoWinningRowToFreq(37,18,40,21,19,7,3,"October 12,2022"); 
+inFunc_setVikingLottoWinningRowToFreq(35,4,44,24,17,41,2,"October 19,2022");
+inFunc_setVikingLottoWinningRowToFreq(17,1,22,23,45,5,5,"November 2,2022"); 
+inFunc_setVikingLottoWinningRowToFreq(22,17,10,18,15,23,3,"November 9,2022");
+inFunc_setVikingLottoWinningRowToFreq(30,46,37,41,9,44,2,"November 16,2022");
+inFunc_setVikingLottoWinningRowToFreq(32,28,27,12,37,5,4,"November 23,2022");
+inFunc_setVikingLottoWinningRowToFreq(22,34,16,40,28,5,5,"November 30,2022");
+inFunc_setVikingLottoWinningRowToFreq(8,13,9,37,10,48,3,"December 7,2022"); 
+inFunc_setVikingLottoWinningRowToFreq(30,2,10,39,22,16,5,"December 14,2022");
+inFunc_setVikingLottoWinningRowToFreq(4,44,23,30,41,6,2,"December 21,2022"); 
+inFunc_setVikingLottoWinningRowToFreq(16,22,24,27,4,12,4,"December 28,2022");
+inFunc_setVikingLottoWinningRowToFreq(1,35,36,22,6,46,1,"January 4,2023");
+inFunc_setVikingLottoWinningRowToFreq(22,47,40,25,43,39,2,"January 11,2023");
+inFunc_setVikingLottoWinningRowToFreq(3,20,41,37,43,26,5,"January 18,2023");
+inFunc_setVikingLottoWinningRowToFreq(18,39,34,19,45,25,2,"January 25,2023");
+inFunc_setVikingLottoWinningRowToFreq(33,26,5,2,20,16,3,"February 1,2023");
+inFunc_setVikingLottoWinningRowToFreq(3,23,4,26,21,34,1,"February 8,2023");
+inFunc_setVikingLottoWinningRowToFreq(48,9,43,41,21,10,4,"February 15,2023");
+inFunc_setVikingLottoWinningRowToFreq(48,9,43,41,21,10,4,"February 22,2023");
